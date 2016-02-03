24e7bb50b84a029788abf496a075cc4a1601eba5
Runar Svendsen <runar.svendsen@gmail.com>
2016-02-03 18:33:00 +0100

    Initial Commit

---
+++ FunctionalViewControllers/FunctionalViewControllers.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalViewControllers.h
+//  FunctionalViewControllers
+//
+//  Created by Runar Svendsen on 03/02/16.
+//  Copyright © 2016 Runar Svendsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalViewControllers.
+FOUNDATION_EXPORT double FunctionalViewControllersVersionNumber;
+
+//! Project version string for FunctionalViewControllers.
+FOUNDATION_EXPORT const unsigned char FunctionalViewControllersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalViewControllers/PublicHeader.h>
+
+
